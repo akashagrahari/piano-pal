d3a7572e052926448733db4df6bfab1475246dc2
Akash Agrahari <akash.agrahari@gmail.com>
2022-04-07 10:42:53 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from './Header.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Homepage from './Homepage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    console.log("yay");
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={
+            <div>
+              <Header></Header>
+              <Homepage></Homepage>
+            </div>
+          }>
+          </Route>
+        </Routes>
+      </BrowserRouter >
+
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/Homepage.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import {
+    Navbar,
+    Container,
+    Nav,
+    NavDropdown,
+    Form,
+    FormControl,
+    Button,
+    NavLink,
+} from 'react-bootstrap'
+import pianobg from './images/piano2.png';
+import {
+    BiFileFind,
+} from 'react-icons/bi'
+
+import {
+    BsUpload,
+    BsMusicNoteBeamed,
+} from 'react-icons/bs'
+
+class Homepage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className='homepage bg-dark'>
+                <Container fluid className='jumbotron text-center heading'>
+                    <div className='container-fluid '>
+                        <h1 className="jumbotron-heading">Piano Pal</h1>
+                        <h2 className=" text-muted">The free open source piano learning tool</h2>
+                    </div>
+
+                    {/* <p>
+                    <link href="#" className=''="btn btn-primary my-2">Main call to action</a>
+                    <link href="#" className=''="btn btn-secondary my-2">Secondary action</a>
+                </p> */}
+                </Container >
+                <img src={pianobg} className='piano-img' alt='...' />
+                <Container fluid className='jumbotron how-to'>
+                    <div className='container-fluid '>
+                        <div className='row'>
+                            <div className='col-lg-4'>
+                                <BiFileFind size={80} />
+                                <div className='how-to-text'>
+                                    <h4 className='text-muted'>Find a MIDI file from sources like <a href='https://freemidi.org/' target='_blank'>FreeMidi</a> </h4>
+                                </div>
+                            </div>
+                            <div className='col-lg-4'>
+                                <BsUpload size={80} />
+                                <div className='how-to-text'>
+                                    <h4 className='text-muted'>Upload the midi file on Piano Pal </h4>
+                                </div>
+                            </div>
+                            <div className='col-lg-4'>
+                                <BsMusicNoteBeamed size={80} scale />
+                                <div className='how-to-text'>
+                                    <h4 className='text-muted'>Play along and learn the song </h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+        )
+    }
+}
+
+export default Homepage;
